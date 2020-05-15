ec2a6f1c8f4b288b079acf0e463274b8dfc6e1c7
Shota Hayashi <qsezy1@hotmail.com>
2020-05-15 12:16:03 +0900

    インデントしてキー配列を変更

--- js/script.js
+++ js/script.js
@@ -1,49 +1,49 @@
 const volumeDiff = 0.01;
 
 window.onload = function() {
-    const backGround = document.getElementById('back-ground');
-    const video = document.getElementById('video');
-    video.volume = 0.5;
-
-    document.addEventListener('keypress', (event) => {
-        switch (event.key) {
-            case 'd':
-                video.play();
-                break;
-            case 'a':
-                video.pause();
-                break;
-            case 'w':
-                if (video.volume < 1)
-                    video.volume = video.volume + volumeDiff;
-                break;
-            case 's':
-                if (video.volume > 0)
-                    video.volume = video.volume - volumeDiff;
-                break;
-            case 'e':
-                if (video.volume < 1)
-                    video.volume = video.volume + volumeDiff * 10;
-                break;
-            case 'q':
-                if (video.volume > 0)
-                    video.volume = video.volume - volumeDiff * 10;
-                break;
-            case 'z':
-                video.volume = 0;
-                break;
-            case 'c':
-                video.volume = 1;
-                break;
-            case 'r':
-                backGround.setAttribute('class', 'back-ground-white');
-                break;
-            case 'f':
-                backGround.setAttribute('class', 'back-ground-black');
-                break;
-            default:
-                console.log(`keypress:${event.key}`);
-                break;
-        };
-    });
+  const backGround = document.getElementById('back-ground');
+  const video = document.getElementById('video');
+  video.volume = 0.5;
+  
+  document.addEventListener('keypress', (event) => {
+    switch (event.key) {
+    case 'd':
+      video.play();
+      break;
+    case 's':
+      video.pause();
+      break;
+    case 'f':
+      if (video.volume < 1)
+        video.volume = video.volume + volumeDiff;
+      break;
+    case 'b':
+      if (video.volume > 0)
+        video.volume = video.volume - volumeDiff;
+      break;
+    case 'p':
+      if (video.volume < 1)
+        video.volume = video.volume + volumeDiff * 10;
+      break;
+    case 'n':
+      if (video.volume > 0)
+        video.volume = video.volume - volumeDiff * 10;
+      break;
+    case 'a':
+      video.volume = 0;
+      break;
+    case 'e':
+      video.volume = 1;
+      break;
+    case 'w':
+      backGround.setAttribute('class', 'back-ground-white');
+      break;
+    case 'q':
+      backGround.setAttribute('class', 'back-ground-black');
+      break;
+    default:
+      console.log(`keypress:${event.key}`);
+      break;
+    };
+  });
 };  
