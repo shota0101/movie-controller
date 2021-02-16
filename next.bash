0ed0d3b2b27e569c78e4cf45863c34114dee29a3
Shota Hayashi <qsezy1@hotmail.com>
2021-02-16 20:16:06 +0900

    ファイルパスを表示するように変更

--- next.bash
+++ next.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd "${HOME}/private/git/movie-controller"
+
 files="./movies/*.mp4"
 for filePath in ${files}; do
+    echo "${filePath}"
     mv "${filePath}" movie.mp4
     break
 done
