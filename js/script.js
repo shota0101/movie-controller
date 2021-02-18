b7dcd483968babed9f923332d2afaab8373c021e
Shota Hayashi <qsezy1@hotmail.com>
2021-02-18 17:36:39 +0900

    レジューム機能の追加

--- js/script.js
+++ js/script.js
@@ -1,31 +1,60 @@
 const volumeDiff = 0.01;
 
+function save() {
+  const video = document.getElementById('video');
+  if (video.currentTime !== 0)
+    localStorage.setItem('movie-resume', video.currentTime);
+}
+
 window.onload = function() {
   const backGround = document.getElementById('back-ground');
   const video = document.getElementById('video');
   video.volume = 0.5;
+  window.setInterval(save, 1000 * 10); // 10秒ごとに保存
   
   document.addEventListener('keypress', (event) => {
     switch (event.key) {
-    case 'd':
+
+      // レジューム関連
+    case 'c':
+      save();
+      alert(video.currentTime / 60);
+      break;
+    case 'r':
+      video.currentTime = localStorage.getItem('movie-resume');
+      break;
+
+      // 停止・再生
+    case 's':
       video.play();
       break;
-    case 's':
+    case 'd':
       video.pause();
+      save();
       break;
+
+      // 早送り
     case 'f':
+      video.currentTime = video.currentTime + 15;
+      break;
+    case 'b':
+      video.currentTime = video.currentTime - 15;
+      break;
+
+      // 音量系
+    case 'p':
       if (video.volume < 1)
         video.volume = video.volume + volumeDiff;
       break;
-    case 'b':
+    case 'n':
       if (video.volume > 0)
         video.volume = video.volume - volumeDiff;
       break;
-    case 'p':
+    case 'P':
       if (video.volume < 1)
         video.volume = video.volume + volumeDiff * 10;
       break;
-    case 'n':
+    case 'N':
       if (video.volume > 0)
         video.volume = video.volume - volumeDiff * 10;
       break;
@@ -35,12 +64,13 @@ window.onload = function() {
     case 'e':
       video.volume = 1;
       break;
-    case 'w':
-      backGround.setAttribute('class', 'back-ground-white');
-      break;
-    case 'q':
-      backGround.setAttribute('class', 'back-ground-black');
-      break;
+      
+    // case 'w':
+    //   backGround.setAttribute('class', 'back-ground-white');
+    //   break;
+    // case 'q':
+    //   backGround.setAttribute('class', 'back-ground-black');
+    //   break;
     default:
       console.log(`keypress:${event.key}`);
       break;
