73b2016879fc2715bd3e5c3c0b5ce0d1ebfa1cb9
Shota Hayashi <shota.hayashi.0101@gmail.com>
2022-09-01 20:50:11 +0900

    movファイルにも対応

--- movie-controller.sh
+++ movie-controller.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-fileName=`ls *.mp4 | peco`
+ls *.mov > temp.txt
+ls *.mp4 >> temp.txt
+fileName=`cat temp.txt | peco`
 echo "file:///Users/lin/Documents/movie-controller.html?path=${fileName}"
 echo "file:///Users/lin/Documents/movie-controller/movie-controller.html?path=${fileName}" | pbcopy
+rm temp.txt
 
