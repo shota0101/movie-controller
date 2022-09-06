0da2e8c5994af11b6d8a47ba790e04d0afd502b0
Shota Hayashi <shota.hayashi.0101@gmail.com>
2022-09-06 20:31:52 +0900

    ファイル名のみをterminalに出力

--- movie-controller.sh
+++ movie-controller.sh
@@ -3,7 +3,7 @@
 ls *.mov > temp.txt 2> /dev/null
 ls *.mp4 >> temp.txt 2> /dev/null
 fileName=`cat temp.txt | peco`
-echo "file:///Users/lin/Documents/movie-controller.html?path=${fileName}"
+echo "${fileName}"
 echo "file:///Users/lin/Documents/movie-controller/movie-controller.html?path=${fileName}" | pbcopy
 rm temp.txt
 
