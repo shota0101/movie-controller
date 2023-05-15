923511115d1927665f014ae0dececacaaf1f8e7a
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-05-16 03:25:55 +0900

    動画ファイルをプロジェクト直下ではなくディレクトリで整理

--- movie-controller.sh
+++ movie-controller.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-temp=${HOME}/temp-movie-controller.txt
-ls *.mov > $temp 2> /dev/null
-ls *.mp4 >> $temp 2> /dev/null
-fileName=`cat $temp | peco`
-echo "${fileName}"
-# echo "file:///Users/lin/Documents/movie-controller/movie-controller.html?path=${fileName}" | pbcopy
-echo "file:///Volumes/GoogleDrive/My%20Drive/air/movie-controller/movie-controller.html?path=${fileName}" | pbcopy
-rm $temp
+filePath=`find m -type f -name "*.mp4" | peco`
+echo "${filePath}"
+echo "file:///Volumes/GoogleDrive/My%20Drive/c/i.html?path=${filePath}" | pbcopy
 
