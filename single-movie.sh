575dca52672522479c365ee9f7a4e7f6462a226e
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-05-17 03:56:22 +0900

    findコマンドの実行結果をキャッシュするように変更

--- single-movie.sh
+++ single-movie.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-filePath=`find m -type f -name "*.mp4" | sort | peco`
+findResult="${HOME}/program-temp/single-movie.txt"
+
+if [ "$(find $findResult -mmin -10 | wc -l)" -eq 0 ]; then
+    echo '10分以上経過してるのでfindコマンドを再実行'
+    find m -type f -name "*.mp4" | sort > $findResult
+fi
+
+filePath=`cat $findResult | peco`
 echo "${filePath}"
 echo "file:///Volumes/GoogleDrive/My%20Drive/c/i.html?path=${filePath}" | pbcopy
 
