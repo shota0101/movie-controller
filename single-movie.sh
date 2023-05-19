53c653db88a545193816c55f42e0b7f736c8a574
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-05-19 21:38:10 +0900

    読み込み時に上書き中だと読めないので処理の順番を修正

--- single-movie.sh
+++ single-movie.sh
@@ -2,12 +2,13 @@
 
 findResult="${HOME}/program-temp/single-movie.txt"
 
+filePath=`cat $findResult | peco`
+
 if [ "$(find $findResult -mmin -10 | wc -l)" -eq 0 ]; then
     echo '10分以上経過してるのでfindコマンドを再実行'
     find m -type f -name "*.mp4" | sort > $findResult &
 fi
 
-filePath=`cat $findResult | peco`
 echo "${filePath}"
 echo "file:///Volumes/GoogleDrive/My%20Drive/c/i.html?path=${filePath}" | pbcopy
 
